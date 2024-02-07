8eca84f23c00410a4cfb16d4c523b11519622e05
Ole M <olesnoen@gmail.com>
2024-02-07 03:33:43 +0100

    Use Heroku  environment variable for port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const developmentOrigins = [
     process.env.FRONTEND_URL,
