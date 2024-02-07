c3923c02519cde4f9bf3c05e6eb44cda5e7a93b3
Ole M <olesnoen@gmail.com>
2024-02-07 16:27:05 +0100

    update: db.js

--- db.js
+++ db.js
@@ -1,9 +1,29 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
-const pool = new Pool({
-    user: process.env.USERNAME,
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DATABASE,
-});
+let dbParams = {};
+if (process.env.DATABASE_URL) {
+    // If running on Heroku, use the Heroku database URL
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(":");
+
+    dbParams = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split("/")[1],
+        ssl: {
+            rejectUnauthorized: false, // Important for Heroku Postgres connection
+        },
+    };
+} else {
+    // If running locally, use the values from .env file
+    dbParams = {
+        user: process.env.USERNAME,
+        host: process.env.HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DATABASE,
+    };
+}
+
+const pool = new Pool(dbParams);
+
 module.exports = pool;
