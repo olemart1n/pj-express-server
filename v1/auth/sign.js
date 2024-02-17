6c217708f01fffab8bc7f5fd5a48e30877fe3004
Ole M <olesnoen@gmail.com>
2024-02-17 20:03:04 +0100

    fix: sign route

--- v1/auth/sign.js
+++ v1/auth/sign.js
@@ -5,16 +5,16 @@ const getCookieOptions = require("../../middleware/cookieOptions");
 const { signUp, findUserByEmail } = require("../../services");
 require("dotenv").config();
 
-router.post("/sign", async (req, res) => {
-    const { username, email, password } = req.body;
+router.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
     const user = await findUserByEmail(email);
     if (user.is_oauth_user) {
         res.status(401).json({ data: null, error: "Feil innloggings-metode" });
         return;
-    } else if (!user && !username) {
+    } else if (!user) {
         res.status(401).json({ data: null, error: "Feil email eller passord" });
         return;
-    } else if (!username && user) {
+    } else {
         const correctPassword = user && bcrypt.compareSync(password, user.password_hash);
         !correctPassword && res.status(401).json({ data: null, error: "Feil email eller passord" });
         const token = jwt.sign({ sub: user.id }, process.env.COOKIE_KEY, {
@@ -26,21 +26,23 @@ router.post("/sign", async (req, res) => {
                 .status(200)
                 .cookie("jwt", token, getCookieOptions())
                 .sendData({ name: user.username, token });
-    } else {
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(password, salt);
+    }
+});
+router.post("/sign-up", async (req, res) => {
+    const { username, email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
-        try {
-            const user = await signUp(username, email, hashedPassword);
-            const token = jwt.sign({ sub: user.id }, process.env.COOKIE_KEY, {
-                expiresIn: "100h",
-            });
-            res.status(201)
-                .cookie("jwt", token, getCookieOptions)
-                .sendData({ name: user.username, token });
-        } catch (err) {
-            res.sendError(err);
-        }
+    try {
+        const user = await signUp(username, email, hashedPassword);
+        const token = jwt.sign({ sub: user.id }, process.env.COOKIE_KEY, {
+            expiresIn: "100h",
+        });
+        res.status(201)
+            .cookie("jwt", token, getCookieOptions())
+            .sendData({ name: user.username, token });
+    } catch (err) {
+        res.sendError(err);
     }
 });
 
